cdd7b95162c523cdf315d13de65b7cd0ece4a59a
Igor Anic <igor.anic@gmail.com>
2011-05-31 09:12:31 +0200

    better handling of shared_files tasks

--- tasks/capistrano_deploy.rb
+++ tasks/capistrano_deploy.rb
@@ -1,13 +1,6 @@
-Capistrano::Configuration.instance(:must_exist).load do 
+Capistrano::Configuration.instance(:must_exist).load do
 
-  #nakon deploya simlinkaj config fileove u /config dir
-  #config fileovi se nalaze u shared/config
-  after "deploy:update_code", "symlink_configs"
-  task :symlink_configs do  
-    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
-  end
-
-  #start stop servisa
+  #start stop deamon
   namespace :deploy do
     desc "Restarting daemon"
     task :restart do
@@ -21,6 +14,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   end
 
   after "deploy:setup", "fix_dir_permissions"
+  desc "change group to deploy_group on dirs created during deploy:setup"
   task :fix_dir_permissions do
     dirs = [deploy_to, releases_path, shared_path]
     dirs += shared_children.map { |d| File.join(shared_path, d) }
@@ -28,12 +22,33 @@ Capistrano::Configuration.instance(:must_exist).load do
     run "#{try_sudo} chgrp #{group} #{dirs.join(' ')}"
   end
 
-  after "deploy:setup", "create_config_yml"
-  task :create_config_yml do
-    dirs = ["#{shared_path}/config"]
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
-    run "#{try_sudo} chgrp #{deploy_group} #{dirs.join(' ')}"
-    run "touch #{shared_path}/config/config.yml"
+  #shared files handling (next three tasks)
+  #shared files are symlinked from shared to the same location in release_path for each deploy
+  #initiali empty files are created during deploy:setup
+  #if file exists in relase_path it is deleted and then replaced with symlink to shared
+  before "deploy:setup", "add_shard_files_to_shared_children"
+  task :add_shard_files_to_shared_children do
+    next unless exists?(:shared_files)
+    shared_files.each do |file|
+      shared_children << File.dirname(file)
+    end
+  end
+
+  after "deploy:setup", "create_shared_files"
+  task :create_shared_files do
+    next unless exists?(:shared_files)
+    shared_files.each  do |file|
+      run "touch #{shared_path}/#{file}"
+    end
+  end
+
+  after "deploy:update_code", "symlink_shared_files"
+  task :symlink_shared_files do
+    next unless exists?(:shared_files)
+    shared_files.each do |file|
+      run "rm -f #{release_path}/#{file}"
+      run "ln -nfs #{shared_path}/#{file} #{release_path}/#{file}"
+    end
   end
 
 end
